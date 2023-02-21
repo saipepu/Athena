8a42ddd2c1718716bdab7305c6e5b7023a672cd1
Jan Poonthong <janpoonthong628@gmail.com>
2023-02-21 16:05:49 +0700

    Seperate form to Question component

---
+++ src/components/StoryBased/Question.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+const Question = ({ textNode, arrayTextNodes, next }) => {
+  // function submitAnswer(e, each, textNode) {
+  //   if (textNode.answer === each.text) {
+  //     setTimeout(() => {
+  //       setNext(each.next)
+  //     }, 500);
+  //   }
+  // }
+
+  return (
+    <div className='questionandanswer'>
+      <h1>{textNode.text}</h1>
+      <div>
+        <form id='submitform'>
+          {textNode.options.map((each, key) => {
+            return (
+              <span key={key}>
+                <input
+                  type="radio"
+                  key={key}
+                  id={key}
+                  onClick={(e) => submitAnswer(e, each, textNode)}
+                  name="radio" />
+                <label
+                  className={textNode.answer === each.text ? 'correctanswer' : 'wronganswer'}
+                  htmlFor={key}>
+                  {each.text}
+                </label>
+              </span>
+            );
+          })}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Question;
