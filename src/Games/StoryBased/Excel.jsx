e43a66d3057eeebed7c7538927b77f591fd97f98
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-05 19:12:36 +0700

    Excel

---
+++ src/Games/StoryBased/Excel.jsx
@@ -0,0 +1,18 @@
+// import XLSX from "xlsx";
+
+const Excel = (e) => {
+  //   const handleFile = async () => {
+  //     const file = e.target.files[0];
+  //     const data = await file.arrayBuffer();
+  //     /* data is an ArrayBuffer */
+  //     const workbook = XLSX.read(data);
+  //   };
+
+  return (
+    <div>
+      <input type="file" />
+    </div>
+  );
+};
+
+export default Excel;
